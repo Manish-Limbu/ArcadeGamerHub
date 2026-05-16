334bdb8509b9ba60e9dd82c6232ae12d6ba52400
manishlimbu <limbumanish792@gmail.com>
2026-05-16 15:36:38 +0545

    feat: new features added

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ import express from "express";
 import db from "./connection/db.js";
 import cors from "cors";
 import dotenv from "dotenv";
-import productRouter from "./router/ProductRoute.js";
+import productRouter from "./router/productRoute.js";
+import userRouter from "./router/userRoute.js";
+import categoryRouter from "./router/categoryRoute.js";
 dotenv.config();
 
 const app = express();
@@ -11,7 +13,9 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-app.use('/api', productRouter);
+app.use('/product', productRouter);
+app.use('/user', userRouter);
+app.use('/category', categoryRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is started at http://localhost:${PORT}`);
